11ab8d17ab16c815f6117e26b451025bde365f2f
Alexander Grube <alexp.grube@googlemail.com>
2022-04-03 12:53:43 +0200

    button and test alert added

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, SafeAreaView, Platform, StatusBar, Image } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Platform, StatusBar, Image, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <Image source={require('./assets/favicon.png')} />
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Image source={require("./assets/favicon.png")} />
+      <Button title="Button" onPress={() => Alert.alert(
+        'Alert Title',
+        'My Alert Msg',
+        [
+          {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+          {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: false }
+      )}/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
